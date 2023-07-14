3f242e5bae8803d0f043815fcd9d4ad1df4b249f
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-07-14 21:34:19 +0300

    add login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,13 @@
 const express = require("express"); //експотруємо express
 
-const { register } = require("../../controllers/users");
+const { register, login } = require("../../controllers/users");
 
 const router = express.Router(); // створюємо роутер
 
 //або signup
 router.post("/register", register);
+
+// або signin
+router.post("/login", login);
 
 module.exports = router; //експортуємо роутер
