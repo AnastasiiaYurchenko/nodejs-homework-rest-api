e3798fd6536e5fa22aa750a7c6bade6c51654d4e
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-07-17 11:13:32 +0300

    fix listContacts add  {owner}

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -3,7 +3,9 @@ const { Contact } = require("../../models/contact");
 const listContacts = async (req, res, next) => {
   try {
     const { _id: owner } = req.user;
-    const result = await Contact.find(owner);
+
+    const result = await Contact.find({ owner });
+
     return res.json(result);
   } catch (error) {
     next(error);
